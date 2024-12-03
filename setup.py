e1567ec904596aa00a6809abd0a591502400aa6b
0nhc <thefoxfoxfox@outlook.com>
2024-12-02 20:44:41 -0600

    add rviz config and achieved slam_toolbox slam

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+"""Setup file for a ROS 2 package."""
+
+from glob import glob
+
+import os
+
 from setuptools import find_packages, setup
 
 package_name = 'nubot_nav'
@@ -10,12 +16,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='zhengxiao-han',
-    maintainer_email='thefoxfoxfox@outlook.com',
-    description='TODO: Package description',
+    maintainer='Zhengxiao Han',
+    maintainer_email='hanzx@u.northwestern.edu',
+    description='This package contains navigation application for nubot.',
     license='WTFPL',
     tests_require=['pytest'],
     entry_points={
